c2fccef49c14fe7889599e0241060f7afb73beee
tiffani <tiffani2k3@gmail.com>
2014-10-12 09:00:22 -0700

    Link to all of the author's posts from one of their solicitation posts.

--- solicitation-content.php
+++ solicitation-content.php
@@ -17,7 +17,7 @@
 <script type="IN/Share" data-url="<?php the_permalink(); ?>" data-counter="right"></script>
 
 <h1><?php the_title(); ?></h1>
-Posted by <?php the_author(); ?> on <?php the_date( 'l, F j, Y' ); ?> at <?php the_time( 'g:i a T' ); ?>.<br /><br />
+Posted by <?php the_author_posts_link(); ?> on <?php the_date( 'l, F j, Y' ); ?> at <?php the_time( 'g:i a T' ); ?>.<br /><br />
 
 <div class="alert-info">
   <?php edit_post_link( 'Edit this post ', '', '' ); ?>
