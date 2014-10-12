36a52fb04910ea1e9a16c6d683d580c133b413bf
tiffani <tiffani2k3@gmail.com>
2014-10-12 08:17:41 -0700

    Beginnings of a 'Solicitations' category page.

---
+++ category-solicitations.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+<div class="page-content">
+  <div class="wrapper">
+    <div style="float: left; width: 200px;">
+      <?php get_sidebar(); ?>
+    </div>
+    <div style="float: right; width: 600px; text-align: left;">
+      <script src="//platform.linkedin.com/in.js" type="text/javascript">
+      /* <![CDATA[ */
+        lang: en_US
+      /* ]]> */
+      </script>
+
+      <!-- THIS IS WHERE THE WORDPRESS CODE TO INCLUDE CONTENT GOES...! -->
+      <p>
+        <h1>Solicitations</h1>
+        We oversee commodity and service procurements for Aviation, Parks, Watershed, Public Works, and General Fund
+        departments. Use the listing below to find all solicitations.
+      </p><br / >
+      <?php
+          if(have_posts()) :
+            while(have_posts() ) : the_post();
+              get_template_part('solicitations-content', get_post_format());
+            endwhile;
+          endif;
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer();
