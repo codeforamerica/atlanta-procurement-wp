3871b8f8b2cfc0a541e11e77fdf049603bc27ba7
tiffani <tiffani2k3@gmail.com>
2014-10-12 08:26:17 -0700

    Amateur! Don't need to build an author link that way.

--- solicitations-content.php
+++ solicitations-content.php
@@ -1,6 +1,6 @@
 <p>
   <h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
   Posted on <?php the_modified_date( 'l, F j, Y' ); ?> at <?php the_modified_time( 'g:i a T' ); ?>
-  by <a href="<?php echo get_the_author_link(); ?>" target="_blank"><?php echo get_the_author_meta('first_name') . " " . get_the_author_meta('last_name'); ?></a><br />
+  by <?php echo get_the_author_link(); ?><br />
   <?php the_excerpt(); ?><br />
 </p>
