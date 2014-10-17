aa362798fca4da08fb1a1e6378d76a27db17b493
tiffani <tiffani2k3@gmail.com>
2014-10-17 14:34:36 -0400

    Have latest solicitations from the last week show up.

--- latest-solicitations.php
+++ latest-solicitations.php
@@ -1,15 +1,17 @@
 <?php
   if ( is_page( 'Home' ) ) { ?>
     <div class="alert alert-info">
-      <h3>Latest opportunities</h3>
+      <h3>Latest opportunities from the last 7 days</h3>
       <ul>
         <?php
-          query_posts('category_name=Solicitations&showposts=3');
-          while (have_posts()) : the_post(); ?>
+          $args = array('category_name' => 'Solicitations', 'date_query' => array(array('after' => '1 week ago')));
+          $posts_query = new WP_Query( $args );
+
+          while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
             <li>
               <a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
             </li>
-        <?php endwhile; wp_reset_query(); ?>
+        <?php endwhile; remove_filter( 'posts_where', 'filter_where' ); wp_reset_query(); ?>
       </ul>
     </div>
 <?php } ?>
