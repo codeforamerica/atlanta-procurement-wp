0877e317f9f0b4e9ec7d297cb3d009f0be311288
tiffani <tiffani2k3@gmail.com>
2014-10-17 14:34:21 -0400

    Get rid of author in byline.

--- solicitations-content.php
+++ solicitations-content.php
@@ -1,6 +1,5 @@
 <p>
   <h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-  Posted on <?php the_modified_date( 'l, F j, Y' ); ?> at <?php the_modified_time( 'g:i a T' ); ?>
-  by <?php echo the_author_posts_link(); ?><br />
+  Posted on <?php the_modified_date( 'l, F j, Y' ); ?> at <?php the_modified_time( 'g:i a T' ); ?><br />
   <?php the_excerpt(); ?><br />
 </p>
