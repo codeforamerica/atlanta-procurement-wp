89e8223f3d9be8c182fbf237e0d866338a14e5cf
tiffani <tiffani2k3@gmail.com>
2014-10-30 10:31:38 -0400

    Archived solicitations link.

--- sidebar.php
+++ sidebar.php
@@ -14,6 +14,7 @@
 
 <strong>What we do</strong><br />
 <a href="<?php echo site_url( '/solicitations/' ); ?>">Commodity &amp; Service Solicitations</a><br />
+<a href="<?php echo site_url( '/archived-solicitations/' ); ?>">Archived Solicitations</a><br />
 <a href="<?php echo site_url( '/surplus-auctions/' ); ?>">Surplus Auctions</a><br />
 <a href="<?php echo site_url( '/supplier-registration/' ); ?>">Supplier Registration</a><br /><br />
 
