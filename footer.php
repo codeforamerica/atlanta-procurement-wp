123507770eec9c9444572a021de23ce9a4169b35
tiffani <tiffani2k3@gmail.com>
2014-11-02 10:55:47 -0800

    Barometer feedback tab added to site.

--- footer.php
+++ footer.php
@@ -66,5 +66,10 @@
     $.backstretch("http://codeforamerica.github.io/atlanta-procurement-www/images/resurgens-opaque.png");
   /* ]]> */
   </script>
+  <style type='text/css'>@import url('http://getbarometer.s3.amazonaws.com/assets/barometer/css/barometer.css');</style>
+  <script src='http://getbarometer.s3.amazonaws.com/assets/barometer/javascripts/barometer.js' type='text/javascript'></script>
+  <script type="text/javascript" charset="utf-8">
+    BAROMETER.load('YE25XK6bGszPKbfk96T1P');
+  </script>
 </body>
 </html>
