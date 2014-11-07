2642d2640bcee6059682fedafea86bcc49ca3e0e
tiffani <tiffani2k3@gmail.com>
2014-11-07 12:50:17 -0800

    Adding SurveyMonkey link.

--- sidebar.php
+++ sidebar.php
@@ -25,4 +25,4 @@
 <a href="<?php echo site_url( '/local-bidder-program/' ); ?>">Local Bidder Information</a><br />
 <a href="<?php echo site_url( '/procurement-code/' ); ?>">Procurement Code</a><br />
 <a href="<?php echo site_url( '/procurement-forms/' ); ?>">Procurement Forms</a><br />
-<a href="#">Supplier Survey</a><br />
+<a href="http://www.surveymonkey.com/r/ATLProcurement" target="_blank">Supplier Survey</a><br />
