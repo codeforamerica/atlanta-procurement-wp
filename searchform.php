b9219c2ae279981ef78e1b4d8fad4dd28b0c6fa0
tiffani <tiffani2k3@gmail.com>
2014-11-09 21:14:15 -0800

    Autofocus because it's just the right thing to do.

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form action="<?php echo home_url(); ?>" method="get">
-		<input class="form-control" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+		<input class="form-control" type="text" name="s" id="search" value="<?php the_search_query(); ?>" autofocus />
 </form>
