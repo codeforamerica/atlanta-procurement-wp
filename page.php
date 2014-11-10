12aae077d5d86a41daa64f4e998c15b17dd8bfce
tiffani <tiffani2k3@gmail.com>
2014-11-09 20:09:30 -0800

    Probably needed for search.

--- page.php
+++ page.php
@@ -15,7 +15,10 @@
       <!-- THIS IS WHERE THE WORDPRESS CODE TO INCLUDE CONTENT GOES...! -->
 
       <?php
-          get_template_part( 'edit_content', get_post_format() );
+          if ( !is_search() ):
+            //get_template_part( 'edit_content', get_post_format() );
+          endif;
+
           get_template_part( 'latest-solicitations', get_post_format() );
 
           if(have_posts()) :
@@ -23,9 +26,7 @@
               the_post();
               the_content();
 
-              if(is_page( 'Solicitations' )) :
-                get_template_part( 'solicitations-dropdown', get_post_format() );
-              endif;
+              get_template_part( 'solicitations-dropdown', get_post_format() );
             endwhile;
           endif;
         ?>
