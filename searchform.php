3ade1099850f7bcdd4ecfb42a61a02ad201f2a57
tiffani <tiffani2k3@gmail.com>
2014-11-09 20:51:50 -0800

    Be explicit about that search URL.

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
-<form action="<?php home_url(); ?>" method="get">
+<form action="<?php echo home_url(); ?>" method="get">
 		<input class="form-control" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
 </form>
