90ca2f7ba8e9f0ec1e30563743d0d8a40b697d1f
tiffani <tiffani2k3@gmail.com>
2014-11-09 21:06:23 -0800

    Tweaks.

--- solicitations-content.php
+++ solicitations-content.php
@@ -1,10 +1,11 @@
 <p>
   <h3><strong><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></strong>
-    <br />
-    <label class="label <?php echo return_solicitation_label(get_field('solicitation_status')); ?>">
-      <?php echo get_field('solicitation_status'); ?>
-    </label>
   </h3>
+  <label class="label <?php echo return_solicitation_label(get_field('solicitation_status')); ?>">
+    <?php echo get_field('solicitation_status'); ?>
+  </label><br />
+</p>
+<p>
   Posted on <?php the_time( get_option( 'date_format' ) ); ?> at <?php the_time( 'g:i a T' ); ?><br />
   Last modified <?php the_modified_date( 'F j, Y' ); ?> at <?php the_modified_time( 'g:i a T' ); ?><br />
   Bids/Proposals Due <?php echo DateTime::createFromFormat('m/d/y g:i a', get_field('bids_due'))->format('l, F j, Y \a\t g:i a'); ?>
