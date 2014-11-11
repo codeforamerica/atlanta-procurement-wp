b698bb959b37150280955082e677ba4fc98d73e6
tiffani <tiffani2k3@gmail.com>
2014-11-10 18:16:06 -0800

    Removing redundant disclaimer.

--- solicitation-content.php
+++ solicitation-content.php
@@ -69,18 +69,7 @@
 </div>
 
 <br /><br />
-<h3>Disclaimer</h3>
-<p>
-  The City of Atlanta (the “City”) does not warrant, represent or guarantee the accuracy or completeness of information provided at this website. Information provided here is simply for your convenience. There may be recent addenda or changes in the Request for Proposals (“RFP”) or bid documents including, among other things, the bidding instructions, general conditions, technical specifications, and exhibits that may not be currently reflected at this site. The City shall not be responsible or liable in any way for errors, inaccuracies or omissions in any documents or information retrieved or downloaded from this site.
-</p>
 
-<p>
-  For a complete set of specifications and the applicable RFP or bid documents, all interested bidders are directed to the City’s Plan Room (“Plan Room”) located at the <a href="https://maps.google.com/?daddr=55+Trinity+Avenue+SW+Atlanta+GA+30303" target="_blank">Department of Procurement, Atlanta City Hall, 55 Trinity Avenue, S. W., Suite 1790, Atlanta, Georgia 30303</a>.
-</p>
-
-<p>
-  In the event of a conflict or discrepancy between the information or documents posted at this website and the RFP or bid documents published and contained in the Plan Room, the RFP or bid documents in the City’s Plan Room shall control.
-</p>
 
 <?php get_template_part( 'csv_export', get_post_format() ); ?>
 <?php get_template_part( 'disclaimer-modal', get_post_format() ); ?>
