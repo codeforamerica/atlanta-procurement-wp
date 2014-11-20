f6bf3dd5c7d5ba0bd987bcb94ed09e442b3cf2ec
tiffani <tiffani2k3@gmail.com>
2014-11-20 14:13:39 -0800

    Verbiage changes.

--- beta-banner.php
+++ beta-banner.php
@@ -1,5 +1,14 @@
 <div class="alert alert-warning">
-  <strong>Note:</strong> You are previewing the new website for the City of Atlanta Department of Procurement. This
-  is a project in transition, however, so there may be changes. Please submit feedback using the <em>Feedback</em> tab on the
-  left of this window.
+  <p>
+    <em><strong>Note:</strong> You are previewing the soon-to-be-launched new website for the City of Atlanta
+    Department of Procurement. This website is still under construction. You can submit feedback on your
+    first impressions of this new site using the Feedback tab on the left of this window.</em>
+  </p>
+  <p>
+    <em>Please continue to visit <a href="www.atlantaga.gov/procurement">www.atlantaga.gov/procurement</a> to browse
+    current procurement opportunities at the City of Atlanta.</em>
+  </p>
+  <p>
+    <em>Thank you!</em>
+  </p>
 </div>
